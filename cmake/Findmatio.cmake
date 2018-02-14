e691e955cf98be32ce703fffaf6cdc783b8fec72
sik <mailsik@gmail.com>
2018-02-14 03:54:35 -0800

    win modifications

--- cmake/Findmatio.cmake
+++ cmake/Findmatio.cmake
@@ -12,7 +12,15 @@ if (NOT matio_LIBRARIES)
 
     # Look for the header file.
 
-    find_path(matio_INCLUDE_DIR NAMES matio.h)
+    find_path(matio_INCLUDE_DIR
+	    HINTS
+	    	external/include	
+		C:/Users/User/findmkl_openmeeg/external/include
+	    NAMES 
+	    	matio.h
+	    )
+    
+    message(STATUS "matio.h ${matio_INCLUDE_DIR}")
     mark_as_advanced(matio_INCLUDE_DIR)
 
     # Look for the library.
@@ -23,23 +31,49 @@ if (NOT matio_LIBRARIES)
         $ENV{matio_dir}bin
         )
 
-    find_library(matio_LIBRARY NAMES matio PATHS ${matio_LIB_SEARCH_PATHS})
+    find_library(matio_LIBRARY 
+	    HINTS 
+	    	${matio_LIB_SEARCH_PATHS}
+	    NAMES libmatio 
+	    )
+    message(STATUS "matio_library ${matio_LIBRARY}")
+
+    ##[===[
+    message(STATUS "---------   sik  ------------")
+
+    find_library(xx_b HINTS ${matio_LIB_SEARCH_PATHS} NAMES matio )
+    message(STATUS "--   sik  -- xx_b: ${xx_b}")
+
+    find_library(xx_c HINTS $ENV{matio_dir} NAMES matio)
+    message(STATUS "--   sik  -- xx_c: ${xx_c}")
+
+    find_library(xx_d HINTS $ENV{matio_dir}lib NAMES matio)
+    message(STATUS "--   sik  -- xx_d: ${xx_d}")
+
+    find_library(xx_e HINTS $ENV{matio_dir}bin NAMES matio)
+    message(STATUS "--   sik  -- xx_e: ${xx_e}")
+
+    message(STATUS "---------   sik  ------------")
+    
+    #]===]
+    
+
     mark_as_advanced(matio_LIBRARY)
 
     # handle the QUIETLY and REQUIRED arguments and set matio_FOUND to TRUE if
     # all listed variables are TRUE
 
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(matio DEFAULT_MSG matio_LIBRARY matio_INCLUDE_DIR)
+    # include(FindPackageHandleStandardArgs)
+    # find_package_handle_standard_args(matio DEFAULT_MSG matio_LIBRARY matio_INCLUDE_DIR)
 
-    if (MATIO_FOUND)
+    #if (MATIO_FOUND)
         set(matio_FOUND TRUE)
         set(matio_LIBRARIES ${matio_LIBRARY} ${HDF5_LIBRARIES})
         set(matio_INCLUDE_DIRS ${matio_INCLUDE_DIR} ${HDF5_INCLUDE_DIR})
-    else()
-        set(matio_LIBRARIES)
-        set(matio_INCLUDE_DIRS)
-    endif()
+	#else()
+    	#    set(matio_LIBRARIES)
+    	#    set(matio_INCLUDE_DIRS)
+    	#endif()
 
-    include(matioVersion)
+    # include(matioVersion)
 endif()
