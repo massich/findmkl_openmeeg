effd826c7a128362ffb227fb441879b49b786163
Joan Massich <sik@visor.udg.edu>
2018-02-16 01:42:01 +0100

    Make MatIO work in windows using conda

--- cmake/Findmatio.cmake
+++ cmake/Findmatio.cmake
@@ -12,28 +12,50 @@ if (NOT matio_LIBRARIES)
 
     # Look for the header file.
 
-    find_path(matio_INCLUDE_DIR NAMES matio.h)
+    find_path(matio_INCLUDE_DIR
+	    HINTS
+        	$ENV{matio_dir}include
+	    NAMES 
+	    	matio.h
+	    )
+    
+    message(STATUS "matio.h ${matio_INCLUDE_DIR}")
     mark_as_advanced(matio_INCLUDE_DIR)
 
     # Look for the library.
 
-    find_library(matio_LIBRARY NAMES matio)
+    set(matio_LIB_SEARCH_PATHS
+        C:/conda/Library/
+        C:/conda/Library/lib
+        C:/conda/Library/bin
+        $ENV{matio_dir}
+        $ENV{matio_dir}lib
+        $ENV{matio_dir}bin
+        )
+
+    find_library(matio_LIBRARY 
+	    HINTS 
+	    	${matio_LIB_SEARCH_PATHS}
+	    NAMES 
+        matio libmatio 
+	    )
+    message(STATUS "matio_library ${matio_LIBRARY}")
     mark_as_advanced(matio_LIBRARY)
 
     # handle the QUIETLY and REQUIRED arguments and set matio_FOUND to TRUE if
     # all listed variables are TRUE
 
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(matio DEFAULT_MSG matio_LIBRARY matio_INCLUDE_DIR)
+    # include(FindPackageHandleStandardArgs)
+    # find_package_handle_standard_args(matio DEFAULT_MSG matio_LIBRARY matio_INCLUDE_DIR)
 
-    if (MATIO_FOUND)
+    #if (MATIO_FOUND)
         set(matio_FOUND TRUE)
         set(matio_LIBRARIES ${matio_LIBRARY} ${HDF5_LIBRARIES})
         set(matio_INCLUDE_DIRS ${matio_INCLUDE_DIR} ${HDF5_INCLUDE_DIR})
-    else()
-        set(matio_LIBRARIES)
-        set(matio_INCLUDE_DIRS)
-    endif()
+	#else()
+    	#    set(matio_LIBRARIES)
+    	#    set(matio_INCLUDE_DIRS)
+    	#endif()
 
-    include(matioVersion)
+    # include(matioVersion)
 endif()
